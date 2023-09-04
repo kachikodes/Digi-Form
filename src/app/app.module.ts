7bf0221001fb3f38f0f85715006534d9180c5dab
Kachi <kachiagu99@gmail.com>
2023-09-04 09:06:54 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,13 @@ import { AflFormComponent } from './afl-form/afl-form.component';
 import { AflTwoComponent } from './afl-two/afl-two.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { EmailTemplateComponent } from './email-template/email-template.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AflFormComponent,
     AflTwoComponent,
-    EmailTemplateComponent
   ],
   imports: [
     BrowserModule,
